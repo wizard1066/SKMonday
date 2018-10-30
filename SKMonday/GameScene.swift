0170a08e4ae9c32c31d9b8194c25edbf0545b483
Mark Lucking <mark.lucking@gmail.com>
2018-10-30 17:04:04 +0100

    Finished game off, very hard right now.

--- SKMonday/GameScene.swift
+++ SKMonday/GameScene.swift
@@ -27,14 +27,45 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var longPress: TimeInterval!
     
     func didBegin(_ contact: SKPhysicsContact) {
-        print("contact \(contact.bodyA.node?.name) \(contact.bodyB.node?.name)")
+        print("contact cA \(contact.bodyA.node?.name) cB \(contact.bodyB.node?.name)")
+        let cA = contact.bodyA.node?.name
+        let cB = contact.bodyB.node?.name
+        if cB == "astroid" && cA == "laser" {
+            splitAstroid(cords: (contact.bodyB.node?.position)!)
+            contact.bodyB.node?.removeFromParent()
+            return
+        }
+        if cA == "astroid" && cB == "laser" {
+            splitAstroid(cords: (contact.bodyB.node?.position)!)
+            contact.bodyA.node?.removeFromParent()
+            return
+        }
+        if cB == "astroidII" && cA == "laser" {
+            contact.bodyB.node?.removeFromParent()
+            return
+        }
+        if cA == "astroidII" && cB == "laser" {
+            contact.bodyA.node?.removeFromParent()
+            return
+        }
+        if cA == "player" && cB == "astroid" {
+            contact.bodyA.node?.removeFromParent()
+            gameover()
+        }
+        if cA == "player" && cB == "astroidII" {
+            contact.bodyA.node?.removeFromParent()
+            gameover()
+        }
     }
     
-//    func didBegin(_ contact: SKPhysicsContact) {
-//        print("contact \(contact.bodyA.node?.name) \(contact.bodyB.node?.name)")
-//        contact.bodyA.node?.removeFromParent()
-//        contact.bodyB.node?.removeFromParent()
-//    }
+    func gameover() {
+        let gameOver = SKLabelNode(fontNamed: "HoeflerText-Italic")
+        gameOver.text = "Game Over"
+        gameOver.fontSize = 65
+        gameOver.fontColor = SKColor.green
+        gameOver.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(gameOver)
+    }
     
     override func didMove(to view: SKView) {
         scene?.physicsWorld.contactDelegate = self
@@ -62,7 +93,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         let gravityVector = vector_float3(0,-1,0)
         gravityNode = SKFieldNode.linearGravityField(withVector: gravityVector)
-        gravityNode!.strength = 1
+        gravityNode!.strength = 2
+        gravityNode!.categoryBitMask = categories.gravityCat
+        gravityNode?.physicsBody?.categoryBitMask = categories.gravityCat
+        gravityNode?.physicsBody?.collisionBitMask = categories.noCat
+        gravityNode?.physicsBody?.contactTestBitMask = categories.noCat
+        gravityNode?.physicsBody?.fieldBitMask = categories.gravityCat
+        gravityNode?.direction = gravityVector
+        gravityNode?.animationSpeed = 1.0
         
         gravityNode!.isEnabled = true
         
@@ -70,48 +108,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = CGVector(dx:0, dy: 0)
         addChild(gravityNode!)
         
-        gravityNode?.physicsBody?.categoryBitMask = categories.gravityCat
-        gravityNode?.physicsBody?.collisionBitMask = categories.noCat
-        gravityNode?.physicsBody?.contactTestBitMask = categories.laserCat
-        
         
         
         let joinMe = SKFieldNode.radialGravityField()
-        joinMe.strength = 1
+        joinMe.strength = 0.25
+        joinMe.falloff = 0
+        joinMe.animationSpeed = 1
         joinMe.isEnabled = true
 
         joinMe.physicsBody?.categoryBitMask = categories.astroidField
         joinMe.physicsBody?.collisionBitMask = categories.noCat
-        joinMe.physicsBody?.contactTestBitMask = categories.playerCat
+        joinMe.physicsBody?.contactTestBitMask = categories.noCat
         joinMe.physicsBody?.fieldBitMask = categories.astroidField
+        joinMe.categoryBitMask = categories.astroidField
         player?.addChild(joinMe)
         
         
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.green
-//            self.addChild(n)
-//        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.blue
-//            self.addChild(n)
-//        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.red
-//            self.addChild(n)
-//        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -122,14 +134,31 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let radiansToDegrees = 180 / CGFloat.pi
         
         
-//        longPress = Date().timeIntervalSinceReferenceDate - 0
-
-//        let angleInRadians = GLKMathDegreesToRadians(6)
-//        let rotateAction = SKAction.rotate(byAngle: CGFloat(angleInRadians), duration: 0)
-//        player?.run(rotateAction)
+        print("touching")
+        
+        let pointTouched = touches.first?.location(in: self.view)
+        
+        if pointTouched!.x < (self.view?.bounds.minX)! + 128 {
+            //            print("LeftSide")
+            let angleInRadians = GLKMathDegreesToRadians(24)
+            let rotateAction = SKAction.rotate(byAngle: CGFloat(angleInRadians), duration: 1)
+            let repeatingAction = SKAction.repeatForever(rotateAction)
+//            player?.run(repeatingAction, withKey: "rotate")
+            player?.run(rotateAction)
+            return
+        }
+        if pointTouched!.x > (self.view?.bounds.maxX)! - 128 {
+            //            print("RightSide")
+            let angleInRadians = GLKMathDegreesToRadians(-24)
+            let rotateAction = SKAction.rotate(byAngle: CGFloat(angleInRadians), duration: 1)
+            let repeatingAction = SKAction.repeatForever(rotateAction)
+//            player?.run(repeatingAction, withKey: "rotate")
+            player?.run(rotateAction)
+            return
+        }
         
         var angleRightNow = (player?.zRotation)!
-//        angleRightNow -= 90 * degreesToRadians
+        angleRightNow -= 180 * degreesToRadians
         let cordX = cos(angleRightNow)
         let cordY = sin(angleRightNow)
         spawnLaser(direction: CGVector(dx: cordX, dy: cordY), angle: angleRightNow)
@@ -137,32 +166,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        player?.removeAction(forKey: "rotate")
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-//        let pressed = Date().timeIntervalSinceReferenceDate - longPress
-//        print("pressed \(pressed)")
-        let angleInRadians = GLKMathDegreesToRadians(6)
-        let rotateAction = SKAction.rotate(byAngle: CGFloat(angleInRadians), duration: 0)
-        player?.run(rotateAction)
+        player?.removeAction(forKey: "rotate")
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        player?.removeAction(forKey: "rotate")
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if (!intersects(astroid!)) {
+            spawnAstroid()
+        }
     }
     
     func spawnLaser(direction: CGVector, angle: CGFloat ){
 //        scene?.physicsWorld.gravity = direction
         gravityNode?.zRotation = angle
         let laserColor:UIColor = UIColor(displayP3Red: 41.0 / 255.0, green: 184.0 / 255.0, blue: 242.0 / 255.0, alpha: 1.0)
-        laser = SKSpriteNode(color: laserColor, size: CGSize(width: 21, height: 21))
+        laser = SKSpriteNode(color: laserColor, size: CGSize(width: 8, height: 8))
         laser?.position = player!.position
         self.addChild(laser!)
         
@@ -189,7 +216,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let randX = GKRandomSource.sharedRandom().nextInt(upperBound: Int(self.view!.bounds.maxX))
         let randY = GKRandomSource.sharedRandom().nextInt(upperBound: Int(self.view!.bounds.maxY))
         astroid?.position = CGPoint(x: randX, y: randY)
-        astroid?.scale(to: CGSize(width: 200, height: 100))
+        astroid?.scale(to: CGSize(width: 100, height: 50))
         astroid?.physicsBody = SKPhysicsBody(rectangleOf: astroid!.size)
         astroid?.physicsBody?.isDynamic = true
         astroid?.physicsBody?.allowsRotation = true
@@ -209,7 +236,50 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(astroid!)
     }
     
-    func splitAstroid() {
-        
+    func splitAstroid(cords: CGPoint) {
+        astroid = SKSpriteNode(imageNamed: "astroid2")
+        let randX2 = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: 128))
+        let randY2 = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: 128))
+        astroid?.position = CGPoint(x: cords.x + randX2, y: cords.y + randY2)
+        astroid?.scale(to: CGSize(width: 100, height: 50))
+        astroid?.physicsBody = SKPhysicsBody(rectangleOf: astroid!.size)
+        astroid?.physicsBody?.isDynamic = true
+        astroid?.physicsBody?.allowsRotation = true
+        astroid?.physicsBody?.affectedByGravity = false
+        astroid?.physicsBody?.friction = 0.0
+        astroid?.physicsBody?.restitution = 0.0
+        astroid?.physicsBody?.angularDamping = 0.0
+        astroid?.physicsBody?.mass = 0.006
+        astroid?.physicsBody?.linearDamping = 0
+        //        laser?.physicsBody?.velocity = CGVector(dx: 0, dy: 500)
+        //        laser?.physicsBody?.velocity = direction
+        astroid?.physicsBody?.categoryBitMask = categories.astroidCat
+        astroid?.physicsBody?.collisionBitMask = categories.noCat
+        astroid?.physicsBody?.contactTestBitMask = categories.playerCat | categories.laserCat
+        astroid?.physicsBody?.fieldBitMask = categories.astroidField
+        astroid?.name = "astroidII"
+        self.addChild(astroid!)
+        astroid = SKSpriteNode(imageNamed: "astroid3")
+        let randX3 = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: 128))
+        let randY3 = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: 128))
+        astroid?.position = CGPoint(x: cords.x + randX3, y: cords.y + randY3)
+        astroid?.scale(to: CGSize(width: 100, height: 50))
+        astroid?.physicsBody = SKPhysicsBody(rectangleOf: astroid!.size)
+        astroid?.physicsBody?.isDynamic = true
+        astroid?.physicsBody?.allowsRotation = true
+        astroid?.physicsBody?.affectedByGravity = false
+        astroid?.physicsBody?.friction = 0.0
+        astroid?.physicsBody?.restitution = 0.0
+        astroid?.physicsBody?.angularDamping = 0.0
+        astroid?.physicsBody?.mass = 0.006
+        astroid?.physicsBody?.linearDamping = 0
+        //        laser?.physicsBody?.velocity = CGVector(dx: 0, dy: 500)
+        //        laser?.physicsBody?.velocity = direction
+        astroid?.physicsBody?.categoryBitMask = categories.astroidCat
+        astroid?.physicsBody?.collisionBitMask = categories.noCat
+        astroid?.physicsBody?.contactTestBitMask = categories.playerCat | categories.laserCat
+        astroid?.physicsBody?.fieldBitMask = categories.astroidField
+        astroid?.name = "astroidII"
+        self.addChild(astroid!)
     }
 }
